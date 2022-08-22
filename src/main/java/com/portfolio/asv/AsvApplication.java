c589e27261e829d377e697bf7f2e070fbc36b7ea
Salome2010 <salomevera32@gmail.com>
2022-08-22 14:50:40 -0300

    Mas cambios

--- src/main/java/com/portfolio/asv/AsvApplication.java
+++ src/main/java/com/portfolio/asv/AsvApplication.java
@@ -2,9 +2,6 @@ package com.portfolio.asv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -12,15 +9,5 @@ public class AsvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsvApplication.class, args);
-	}
-        
-        @Bean
-   public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("https://frontendasvv.web.app/").allowedHeaders("*").allowedMethods("*");
-			}
-		};
-	}
+        }
 }
