c2a17602fb169821fbe719fffdda8f80bfd21094
Salome2010 <salomevera32@gmail.com>
2022-08-22 20:01:55 -0300

    Arreglos

--- src/main/java/com/portfolio/asv/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/asv/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontendasvv.web.app")
+@CrossOrigin(origins = {"https://frontendasvv.web.app"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
