675793e229ad115fb90bccefd4a8a24bca45eafc
Salome2010 <salomevera32@gmail.com>
2022-08-23 11:46:32 -0300

    Cambios para cors

--- src/main/java/com/portfolio/asv/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/asv/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200", "https://frontendasvv.web.app"})
+@CrossOrigin(origins ="https://frontendasvv.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
