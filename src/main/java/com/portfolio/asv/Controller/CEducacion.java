81f2a0a4ef8d4d3bbb1a0f9b441765f67ef0d45a
Salome2010 <salomevera32@gmail.com>
2022-10-19 20:47:23 -0300

    Cambio de pagina

--- src/main/java/com/portfolio/asv/Controller/CEducacion.java
+++ src/main/java/com/portfolio/asv/Controller/CEducacion.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins ="https://frontendasvv.web.app")
+@CrossOrigin(origins ="https://frontendasv.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portfolio/asv/Controller/PersonaController.java
+++ src/main/java/com/portfolio/asv/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins ="https://frontendasvv.web.app")
+@CrossOrigin(origins ="https://frontendasv.web.app")
 public class PersonaController {
 
     @Autowired
