5ec0240cbc2f82930180a0c454d8f61941ece8a6
Salome2010 <salomevera32@gmail.com>
2022-10-20 13:21:17 -0300

    Url nueva

--- src/main/java/com/portfolio/asv/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/asv/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins ="https://frontendasv.web.app")
+@CrossOrigin //(origins ="https://frontendasv.web.app")
 public class AuthController {
 
     @Autowired
