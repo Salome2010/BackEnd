c4c4637fbb6f511b4055671bcd6bee89f9abb15c
Salome2010 <salomevera32@gmail.com>
2022-10-25 19:16:45 -0300

    Pruebas

--- src/main/java/com/portfolio/asv/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/asv/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")g
+@RequestMapping("/auth")
 @CrossOrigin(origins ="https://frontendasv.web.app")
 public class AuthController {
 
